6175d10fd5f2fe1b3a1de71e1f072ab514230401
Vishal chaturvedi <vc121356@gmail.com>
2019-09-25 01:24:17 +0530

    implement TOCropViewController

--- ios/src/ImageCropPicker.h
+++ ios/src/ImageCropPicker.h
@@ -29,6 +29,8 @@
 #import <RSKImageCropper/RSKImageCropper.h>
 #endif
 
+#import <TOCropViewController/TOCropViewController.h>
+
 #import "UIImage+Resize.h"
 #import "Compression.h"
 #import <math.h>
@@ -39,7 +41,8 @@ UINavigationControllerDelegate,
 RCTBridgeModule,
 QBImagePickerControllerDelegate,
 RSKImageCropViewControllerDelegate,
-RSKImageCropViewControllerDataSource>
+RSKImageCropViewControllerDataSource,
+TOCropViewControllerDelegate>
 
 typedef enum selectionMode {
     CAMERA,
